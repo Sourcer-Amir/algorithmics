110824872faf7e1b5e23e4d6cc547b7026d8d040
Sourcer_A <santiro.amir@gmail.com>
2025-09-20 19:31:55 -0600

    Good structure,nice stuff

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 //I read that we musn't use libraries
 //We can use the standard libraries like <iostream> and <vector>, also these ones 
+//Change the primary key to "timeSum" or something like that
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -8,54 +9,39 @@
 using namespace std;
 
 struct entry{
-    int month; //It's an integer between 1 and 12
-    int day;
-    int hour;
-    int minute;
-    int second;
+    int month, day, hour, minute, second;
+    long long primarykey; //Is not a real primary key, but it's similar Dan 
     int ip1, ip2, ip3, ip4; //Cant be a string because we need to compare them
     int port;
-    string reason;
+    string reason; //The last part of the log entry
     string originLine; //I think this will be useful for printing the querie
+};
+
+int months_int(string& month){
+    string months [12] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+    for(int i = 0; i < 12; i++)
+        if(months[i] == month) return i + 1;
+    return -1;
 }
 
-//I re-use the merge sort of the last code
-void merge(int a[], int l, int m, int r, long long &comp ){
-    int n1 = m - l + 1;
-    int n2 = r - m;
+long long primaryKey(int month, int day, int hour, int minute, int second){
+    return (((((month * 31LL + day) * 24 + hour) * 60 + minute) * 60) + second);
+} // I already know that is not a primary key, not unique, but it's pretty similar
 
-    int *L = new int[n1];
-    int *R = new int[n2];
 
-    for (int i = 0; i < n1; ++i) L[i] = a[l + i];
-    for (int j = 0; j < n2; ++j) R[j] = a[m + 1 + j];
-
-    int i = 0, j = 0, k = l;
-    while (i < n1 && j < n2) {
-        comp++;                    
-        if (L[i] <= R[j]) a[k++] = L[i++];
-        else               a[k++] = R[j++];
-    }
-    while (i < n1) a[k++] = L[i++];
-    while (j < n2) a[k++] = R[j++];
-
-    delete[] L; delete[] R;
-}
-
-void mergeSort(int a[], int l, int r, long long &comp){
-    if (l >= r) return;
-    int m = l + (r - l) / 2;
-    mergeSort(a, l, m, comp);
-    mergeSort(a, m + 1, r, comp);
-    merge(a, l, m, r, comp);
-}
-
-void ordenaMerge(int a[], int n, long long &comp) {
-    comp = 0; 
-    if (n > 0) mergeSort(a, 0, n - 1, comp);
-}
 
 int main(){
-    
+    ifstream theFile("bitacora.txt");
+    vector<entry> logs;
+    string line;
+    while(getline(theFile,line)){
+        entry TO; //This is a temproraly object to store the data
+        size_t pos = 0; //I use this data type because the function find return this type 
+
+        //TO.originLine = line;
+        logs.push_back(TO);
+    }
+    cout << logs.size() << "\n";
+
     return 0;
 }
