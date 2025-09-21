252c2b5506fcf7cdfcea6ac74d8e8beaf0c112e4
Sourcer_A <santiro.amir@gmail.com>
2025-09-20 23:21:09 -0600

    Try to do the split IP

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //I read that we musn't use libraries
 //We can use the standard libraries like <iostream> and <vector>, also these ones 
-//Change the primary key to "timeSum" or something like that
+
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -9,8 +9,8 @@
 using namespace std;
 
 struct entry{
-    int month, day, hour, minute, second;
-    long long totalTime; //Is not a real primary key, but it's similar Dan 
+    int month, day, hour, min, sec;
+    long long totalTime; 
     int ip1, ip2, ip3, ip4; //Cant be a string because we need to compare them
     int port;
     string reason; //The last part of the log entry
@@ -27,14 +27,32 @@ int months_int(string& month){
 
 string tokenizer(string& line, size_t &pos){
     size_t n = line.size();
-    while (pos < n && line[pos] == ' ') ++pos;
+    while (pos < n && line[pos] == ' ') 
+        ++pos;
+        if(pos >=n)
+            return "";
+        size_t start = pos;
+        while (pos < n && line[pos] != ' ')
+            ++pos;
+        return line.substr(start, pos - start);
 }
 
+void splitIp(string &i, int &a, int &b, int &c, int &d, int &p){
+    string ip = i.substr(0,i.find(":"));
+    size_t dot = i.find(".");
+    string parts = ip.substr(0,dot);
+    a = stoi(parts);
 
-long long primaryKey(int month, int day, int hour, int minute, int second){
+    a = stoi(i.substr(0,a));
+    b = stoi(i.substr(a + 1, b - a - 1));
+    c = stoi(i.substr(b + 1));
+    d = stoi(i.substr(c + 1, p1 - c - 1));
+    p = stoi(i.substr(p1 + 1));
+}
+
+long long total_time(int month, int day, int hour, int minute, int second){
     return (((((month * 31LL + day) * 24 + hour) * 60 + minute) * 60) + second);
 } // I already know that is not a primary key, not unique, but it's pretty similar
-
 
 
 int main(){
@@ -44,7 +62,18 @@ int main(){
     while(getline(theFile,line)){
         entry TO; //This is a temporaly object to store the data
         size_t pos = 0; //I use this data type because the function find return this type 
-
+        string month_str = tokenizer(line,pos);
+        string day_str = tokenizer(line, pos);
+        string time_str = tokenizer(line, pos);
+        
+        TO.month  = months_int(month_str);
+        TO.day = stoi(day_str);
+        TO.hour = stoi(time_str.substr(0,2));
+        TO.min = stoi(time_str.substr(3,4));
+        TO.sec = stoi(time_str.substr(6,2));
+        
+        TO.totalTime = total_time(TO.month, TO.day, TO.hour,  TO.min, TO.sec);
+     
         //TO.originLine = line;
         logs.push_back(TO);
     }
